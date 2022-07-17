fa6fb403ecfad6a0091979fc2b232daed9313eef
Henrique-Rmc <paulo.cruz@ccc.ufcg.edu.br>
2022-07-17 12:20:58 -0300

    Finalizado?

--- Util.hs
+++ Util.hs
@@ -59,7 +59,7 @@ isValid n (x:xs) | ((aux n x) == False) = isValid n xs
 -----------------------------------------------------------------
 getListaMercadosTxt :: [[String]] -> String
 getListaMercadosTxt [] = ""
-getListaMercadosTxt (x:xs) = head x ++ (x !! 1) ++ "\n" ++ getListaMercadosTxt xs
+getListaMercadosTxt (x:xs) = head x ++ "," ++ (x !! 1) ++ "\n" ++ getListaMercadosTxt xs
 
 
 
